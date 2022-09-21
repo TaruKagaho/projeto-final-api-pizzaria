bdbbe5156015ac7fd6ad7157d64e18abeb627a88
Adir Silva Filho <tarusilva@gmail.com>
2022-09-21 20:15:00 -0300

    feat: cria controladores para as rotas customers
    Cria os seguintes controladores:
    
    - addNewCustomer para criar um novo cliente e salva as informações no
    banco de dados;
    
    - getAllCustomers para obter a lista de clientes cadastrados no
    banco de dados;
    
    - getCustomerById para obter as informações do cliente correspondente
    ao id params informado no banco de dados;
    
    - updateCustomer para atualizar as informações do cliente
    correspondente ao id params informado no banco de dados; e
    
    - deleteCustomer para apagar o cliente correspondente ao id params
    informado do banco de dados.

---
+++ src/controllers/customers.js
@@ -0,0 +1,108 @@
+const bcrypt = require( "bcryptjs" );
+
+const { db } = require( "../database/connection" );
+
+const { Customers } = require( "../models" );
+
+const addNewCustomer = async ( req, res ) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const cryptPassword = await bcrypt.hash( password, 10 );
+
+        // @ts-ignore
+        const newCustomer = await Customers.create( {
+            name,
+            email,
+            password: cryptPassword,
+        } );
+
+        return res.status( 201 ).json( newCustomer );
+    } catch ( error ) {
+        console.log( error.message );
+    }
+};
+
+const getAllCustomers = async ( req, res ) => {
+    try {
+        // @ts-ignore
+        // const customersList = await Customers.findAll();
+
+        const query = "SELECT id, name, email FROM customers";
+        const [ results ] = await db.query( query );
+
+        return res.status( 200 ).json( results );
+    } catch ( error ) {
+        console.log( error.message );
+    }
+};
+
+const getCustomerById = async ( req, res ) => {
+    try {
+        const { customer } = req;
+
+        return res.status( 200 ).json( customer );
+    } catch ( error ) {
+        console.log( error.message );
+    }
+};
+
+const updateCustomer = async ( req, res ) => {
+    try {
+        const { id } = req.params;
+        const { name, email, password } = req.body;
+        const { customer } = req;
+        
+        let cryptPassword;
+        
+        if ( password ) {
+            cryptPassword = await bcrypt.hash( password, 10 );
+        }
+        // @ts-ignore
+        await Customers.update( {
+            name,
+            email,
+            password: cryptPassword,
+        }, {
+            where: {
+                id,
+            }
+        } );
+
+        const customerUpdated = {
+            id: customer.id,
+            name: name ? name : customer.name,
+            email: email ? email : customer.email,
+        };
+        
+        return res.status( 200 ).json( customerUpdated );
+    } catch ( error ) {
+        console.log( error.message );
+    }
+};
+
+const deleteCustomer = async ( req, res ) => {
+    try {
+        const { id } = req.params;
+
+        // @ts-ignore
+        await Customers.destroy( {
+            where: {
+                id,
+            }
+        } );
+
+        // return res.status( 204 );
+        return res.status( 200 ).json( "Cliente removido com sucesso" );
+    } catch ( error ) {
+        console.log( error.message );
+    }
+};
+
+module.exports = {
+    addNewCustomer,
+    getAllCustomers,
+    getCustomerById,
+    updateCustomer,
+    deleteCustomer,
+};
